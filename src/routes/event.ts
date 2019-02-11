fc9331e280ae58605b14edba78c14245abfc77e1
Hendrik Folkerts <hendrik.folkerts@student.hpi.uni-potsdam.de>
2019-02-11 13:27:48 +0100

    Use updated getEvents function

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -5,8 +5,9 @@ const router: express.Router = express.Router();
 
 router.get('/:topic', async (req, res) => {
   const topic = req.params.topic;
+  const minLevel = req.query['min-level'];
 
-  const events = await EventService.getEvents(topic);
+  const events = await EventService.getEvents(topic, minLevel);
   res.send(events);
 });
 
