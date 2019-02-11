4b5fd5ce3ff89cc9c4611915ba9d216817e4fe6b
Hendrik Folkerts <hendrik.folkerts@student.hpi.uni-potsdam.de>
2019-02-11 14:16:08 +0100

    Parse event details

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,13 +1,22 @@
 import axios from 'axios';
 import config from '@/config';
 
+const parseJSON = (a: string) => JSON.parse(a.replace(/&#34;/g, '"'));
+
 export default class EventService {
   // region public static methods
   public static async getEvents(topic: string, minLevel: string): Promise<any> {
     try {
-      const response = await axios.get(`${config.kapacitorURL}/kapacitor/v1/alerts/topics/${topic}/events\
-      ?min-level=${minLevel}`);
-      return response.data;
+      const response = await axios.get(`${config.kapacitorURL}/kapacitor/v1/alerts/topics/${topic}/events?min-level=${minLevel}`);
+
+      const responseData = response.data;
+
+      responseData.events.forEach((event: any) => {
+        event.state.details = parseJSON(event.state.details);
+        console.log(event.state.details);
+      });
+
+      return responseData;
     } catch (error) {
       return error.response.data;
     }
