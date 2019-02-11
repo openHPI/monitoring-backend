50c85e30baa24709a9fb440dae9a79858c826596
Hendrik Folkerts <hendrik.folkerts@student.hpi.uni-potsdam.de>
2019-02-11 13:27:37 +0100

    Consider minLevel for events

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -3,9 +3,10 @@ import config from '@/config';
 
 export default class EventService {
   // region public static methods
-  public static async getEvents(topic: string): Promise<any> {
+  public static async getEvents(topic: string, minLevel: string): Promise<any> {
     try {
-      const response = await axios.get(`${config.kapacitorURL}/kapacitor/v1/alerts/topics/${topic}/events`);
+      const response = await axios.get(`${config.kapacitorURL}/kapacitor/v1/alerts/topics/${topic}/events\
+      ?min-level=${minLevel}`);
       return response.data;
     } catch (error) {
       return error.response.data;
